6433c89dad2813b714ff76e555eaccea2ceeacc3
Brecht De Ruyte <deruytebrecht@gmail.com>
2025-09-09 09:13:16 +0200

    examples update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // This config is now set up to build the index.html as a static site for GitHub Pages.
 export default defineConfig({
     // The base path for the deployed site. This should match your GitHub repository name.
-    base: '/enhanced-range-slider-poc/',
+    base: "/enhanced-range-slider-poc/",
     build: {
         // The output directory for the build is 'dist' by default, which is
         // what the GitHub Pages workflow expects.
