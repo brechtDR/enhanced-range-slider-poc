25de15f3163a0d9b3b718e7d4115ef4ec563d647
Brecht De Ruyte <deruytebrecht@gmail.com>
2025-09-10 20:09:48 +0200

    remove old things

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 
-// This config is now set up to build the index.html as a static site for GitHub Pages.
 export default defineConfig({
-  // The base path for the deployed site. This should match your GitHub repository name.
   base: '/enhanced-range-slider-poc/',
   build: {
-    // The output directory for the build is 'dist' by default, which is
-    // what the GitHub Pages workflow expects.
   },
 });
